62d2c8a2ed15e691cd0a1def1e09cd816de8721b
Wladimir <w.reimer@live.de>
2022-12-07 10:29:45 +0100

    moved terraform files to infrastructure

---
+++ infrastructure/dynamodb.tf
@@ -0,0 +1,67 @@
+locals {
+  postaljson = file("./postalcode.json")
+  postaldata = jsondecode(local.postaljson)
+}
+
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+    name           = "CapstoneDB"
+    billing_mode   = "PROVISIONED"
+    read_capacity  = 20
+    write_capacity = 20
+    hash_key       = "id"
+    range_key      = "dist"
+
+    attribute {
+            name = "id"
+            type = "S"
+    }
+    attribute {
+            name = "dist"
+            type = "N"
+    }
+
+    global_secondary_index {
+        name               = "Index"
+        hash_key           = "id"
+        range_key          = "dist"
+        write_capacity     = 10
+        read_capacity      = 10
+        projection_type    = "INCLUDE"
+        non_key_attributes = ["Bla"]
+    }
+}
+
+resource "aws_dynamodb_table" "postalcodelist" {
+    name           = "PoCpostalcodelist"
+    billing_mode   = "PROVISIONED"
+    read_capacity  = 20
+    write_capacity = 20
+    hash_key       = "postalcode"
+    range_key      = "city"
+
+    attribute {
+            name = "postalcode"
+            type = "S"
+    }
+    attribute {
+            name = "city"
+            type = "S"
+    }
+
+    global_secondary_index {
+        name               = "Index"
+        hash_key           = "postalcode"
+        range_key          = "city"
+        projection_type    = "INCLUDE"
+        write_capacity     = 10
+        read_capacity      = 10
+        non_key_attributes = ["lng", "lat"]
+    }
+}
+resource "aws_dynamodb_table_item" "postalcodedata" {
+  for_each = local.postaldata
+  table_name = aws_dynamodb_table.postalcodelist.name
+  hash_key   = aws_dynamodb_table.postalcodelist.hash_key
+  range_key  = aws_dynamodb_table.postalcodelist.range_key
+  item = jsonencode(each.value)
+}
