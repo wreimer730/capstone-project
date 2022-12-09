735a012c596b43e178fd8be3add5e373f9fc92c2
Wladimir <w.reimer@live.de>
2022-12-09 19:33:25 +0100

    changed path to lambda/zip

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -5,7 +5,7 @@ data "aws_iam_role" "labrole" {
 resource "aws_lambda_function" "test_lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
-  filename      = "src/capstone-lambda.zip"
+  filename      = "../src/zip/capstone-lambda.zip"
   function_name = "capstone-lambda"
   role          = data.aws_iam_role.labrole.arn
   handler       = "lambda_function.lambda_handler"
@@ -15,7 +15,7 @@ resource "aws_lambda_function" "test_lambda" {
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = filebase64sha256("src/capstone-lambda.zip")
+  source_code_hash = filebase64sha256("../src/zip/capstone-lambda.zip")
 
   runtime = "python3.9"
 }
