ba7220558b3587416a5471d3e185526bfc025773
Wladimir <w.reimer@live.de>
2022-12-19 20:11:14 +0100

    small adjustments for infrastructure

--- src/capstone-api/main.py
+++ src/capstone-api/main.py
@@ -1,27 +1,32 @@
+# this code will take all data from dynamodb
 import boto3
 from fastapi import FastAPI
 
 capstone = FastAPI()
 
+session = boto3.Session()
+credentials = session.get_credentials()
+
 client = boto3.client('dynamodb',
-  aws_access_key_id='ASIAUKNDBQNGML5AZRWU',
-  aws_secret_access_key='V/Qp3YHwEomknVDrgM0BkX0vm/FkAhZFPkWndt8l',
-  region_name='us-west-2')
+    aws_access_key_id=credentials.access_key,
+    aws_secret_access_key=credentials.secret_key,
+    region_name='us-west-2')
+                      
 
 dynamodb = boto3.resource('dynamodb', region_name="us-west-2")
 table = dynamodb.Table('capstoneDB')
-response = table.scan()
 
+response = table.scan()
 list = response['Items']
 
-@capstone.get("/stations")
-async def jobs():
+@capstone.get("/stations") 
+async def places():
     return list
 
-@capstone.get("/station/{item}")
-async def getjob(item):
+@capstone.get("/station/{place}")
+async def getplace(place):
     mylist = []
     for keyvalue in list:
-        if keyvalue["place"] == item:
+        if keyvalue["place"] == place:
             mylist.append(keyvalue)
     return mylist
