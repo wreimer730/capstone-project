7422c651468c23778db149051fcf6b00df3ce6f6
Wladimir <w.reimer@live.de>
2022-12-20 09:55:12 +0100

    changed url-path station to stations

--- src/capstone-api/main.py
+++ src/capstone-api/main.py
@@ -11,20 +11,20 @@ client = boto3.client('dynamodb',
     aws_access_key_id=credentials.access_key,
     aws_secret_access_key=credentials.secret_key,
     region_name='us-west-2')
-                      
 
 dynamodb = boto3.resource('dynamodb', region_name="us-west-2")
 table = dynamodb.Table('capstoneDB')
 
-response = table.scan()
-list = response['Items']
-
 @capstone.get("/stations") 
 async def places():
+    response = table.scan()
+    list = response['Items']
     return list
 
-@capstone.get("/station/{place}")
+@capstone.get("/stations/{place}")
 async def getplace(place):
+    response = table.scan()
+    list = response['Items']
     mylist = []
     for keyvalue in list:
         if keyvalue["place"] == place:
